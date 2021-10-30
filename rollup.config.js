a555f7b2925a1417f6827b309347bec4f4e01c9b
Sanjeet Tiwari <sanjeettiwari00076@gmail.com>
2021-10-31 02:56:50 +0530

    Add support for react

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,10 @@
 import serve from 'rollup-plugin-serve';
 import livereload from 'rollup-plugin-livereload';
+import babel from '@rollup/plugin-babel';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
+import postcss from 'rollup-plugin-postcss';
 
 export default {
     input: 'src/index.js',
@@ -9,11 +14,24 @@ export default {
         sourcemap: true
     },
     plugins: [
+        nodeResolve({
+            extensions: ['.jsx', '.js']
+        }),
+        replace({
+            'process.env.NODE_ENV': JSON.stringify('development'),
+            preventAssignment: true
+        }),
+        babel({
+            exclude: 'node_modules/**',
+            presets: ["@babel/preset-react"]
+        }),
+        commonjs(),
         serve({
             open: true,
             contentBase: ["", "public"],
             port: 3000,
         }),
-        livereload({ watch: 'dist' })
+        livereload({ watch: 'dist' }),
+        postcss({ autoModules: true })
     ]
 }
---
+++ src/components/Hello.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from 'Hello.module.scss';
+
+const Hello = () => {
+    return <h1 className={styles.name}>Hello</h1>
+}
+
+export default Hello;
